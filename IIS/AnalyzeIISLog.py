a3449de5490d8b53d10215260d517bf09349c2db
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2017-06-21 14:13:18 +0800

    update IIS/AnalyzeIISLog.py

--- IIS/AnalyzeIISLog.py
+++ IIS/AnalyzeIISLog.py
@@ -1,5 +1,5 @@
-DATE = '170618'
-LOG_DIR = r'C:\Users\wenw\Desktop\0619\132\log'
+DATE = '170620'
+LOG_DIR = r'C:\Users\wenw\Desktop\117041115583204-Opera-IIS-Hang\log'
 STAT_DIR = r'C:\Users\wenw\Desktop\IIS-log'
 
 import os
@@ -14,30 +14,27 @@ bCounter = Counter(bList)
 cList = [r':'.join(line.split(r':')[:2]) for line in bList]
 cCounter = Counter(cList)
 
-'''
-secStat = open(r'C:\Users\wenw\Desktop\IIS-log\170606-sec.csv', 'w')
+secStat = open(os.path.join(STAT_DIR, r'%s-sec.csv' % DATE), 'w')
 for k in sorted(bCounter):
     secStat.write('%s, %s' % (k, bCounter[k]) + '\n')
 
-secStat = open(r'C:\Users\wenw\Desktop\IIS-log\170606-min.csv', 'w')
+secStat = open(os.path.join(STAT_DIR, r'%s-min.csv' % DATE), 'w')
 for k in sorted(cCounter):
     secStat.write('%s, %s' % (k, cCounter[k]) + '\n')
 
 dList = [line[1] for line in aList if int(line[-1]) > 10000]
 dList = [r':'.join(line.split(r':')[:2]) for line in dList]
 dCounter = Counter(dList)
-secStat = open(r'C:\Users\wenw\Desktop\IIS-log\170606-10Sec.csv', 'w')
+secStat = open(os.path.join(STAT_DIR, r'%s-10sec.csv' % DATE), 'w')
 for k in sorted(dCounter):
     secStat.write('%s, %s' % (k, dCounter[k]) + '\n')
 
 dList = [line[1] for line in aList if int(line[-1]) > 30000]
 dList = [r':'.join(line.split(r':')[:2]) for line in dList]
 dCounter = Counter(dList)
-secStat = open(r'C:\Users\wenw\Desktop\IIS-log\170606-30Sec.csv', 'w')
+secStat = open(os.path.join(STAT_DIR, r'%s-30sec.csv' % DATE), 'w')
 for k in sorted(dCounter):
     secStat.write('%s, %s' % (k, dCounter[k]) + '\n')
-'''
-
 
 aDict = {}
 for line in aList:
@@ -60,7 +57,6 @@ for k in sorted(bDict): #, key=(lamba x:bDict[x][0]), reverse=True):
     v = ['%s' % i for i in bDict[k]]
     v = ', '.join(v)
     secStat.write('%s, %s' % (k, v) + '\n')
-
 
 aDict = {}
 for line in aList:
